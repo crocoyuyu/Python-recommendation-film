ac991d413774ca440b49e15b489ff3fb7ee2b450
Yuan <dingyuan0525@outlook.com>
2026-08-03 17:53:02 +0200

    Installation de streamlit et test initial

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("Hello")
+import streamlit as st
+
+st.title("Système de recommandation dpython -m streamlit run app.pye films")
+
+st.write("Streamlit fonctionne correctement !")
+
+film = st.text_input("Entre le nom d'un film que tu aimes")
+
+if film:
+    st.success(f"Nous chercherons des films similaires à : {film}")
